8cb14d3d976556b20749156a87b0cb9a94c7c8fe
Your Name <you@example.com>
2022-01-16 18:56:29 +0530

    coin_change1 committed

---
+++ coin_change1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ppi pair<int,int>
+#define lli long long int
+#define li long int
+#define vi vector<int>
+#define vlli vector<lli>
+#define vli vector<li>
+#define merge(a,b) a##b
+#define get(a) #a
+#define rep(i,k,n) for (i = k; i < n; ++i)
+#define Repr(i,k,n) for (i = k; i >= n; --i)
+#define c(n) cout<<n<<endl
+#define z(n) cout<<n<<" "
+#define nl cout<<endl;
+#define INF INT_MAX
+using namespace std;
+
+int main(){
+    c("sukhman");
+    return 0;
+}
